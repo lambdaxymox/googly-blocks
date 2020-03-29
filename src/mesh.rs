f1fa0d4426e380958c2a719e7f9f247686fbad74
LambdaXymox <lambda.xymox@gmail.com>
2020-03-28 23:42:10 -0500

    Migrate to GPL license.

--- src/mesh.rs
+++ src/mesh.rs
@@ -1,3 +1,20 @@
+/*
+ *  Googly Blocks is a video game.
+ *  Copyright (C) 2018,2019,2029  Christopher Blanchard
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 use std::mem;
 
 
