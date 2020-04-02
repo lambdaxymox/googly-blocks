889bb6ac8513a62b661aa9b6078cd9c98c7d79b0
LambdaXymox <lambda.xymox@gmail.com>
2020-04-01 22:24:00 -0500

    Implement right wall collision detection.

--- src/board.rs
+++ src/board.rs
@@ -495,7 +495,15 @@ fn collides_with_left_wall(piece: GooglyBlock, top_left: BlockPosition, landed: 
 }
 
 fn collides_with_right_wall(piece: GooglyBlock, top_left: BlockPosition, landed: &LandedBlocks) -> bool {
-    unimplemented!();
+    let shape = piece.shape();
+    for (_, column) in shape.iter() {
+        let element_column = column as isize;
+        if top_left.column + element_column >= landed.columns() as isize {
+            return true;
+        }
+    }
+
+    false
 }
 
 
