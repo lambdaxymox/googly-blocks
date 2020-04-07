ab57c5d72883fcf4255f617c440ced3f3cd22aea
LambdaXymox <lambda.xymox@gmail.com>
2020-04-07 02:00:34 -0500

    Implement more of the block positioning updating such that right wall collisions are accounted for.

--- src/playing_field.rs
+++ src/playing_field.rs
@@ -568,6 +568,16 @@ impl PlayingFieldState {
                     self.current_position = potential_top_left;
                 }
             }
+            GooglyBlockMove::Right => {
+                let potential_top_left = BlockPosition { row: self.current_position.row, column: self.current_position.column + 1 };
+                let collides_with_element = collides_with_element(self.current_block, potential_top_left, &self.landed_blocks);
+                let collides_with_right_wall = collides_with_floor(self.current_block, potential_top_left, &self.landed_blocks);
+                if collides_with_element || collides_with_right_wall {
+
+                } else {
+                    self.current_position = potential_top_left;
+                }
+            }
             _ => {},
         }
     }
